9fb80456ac0b59357e1f3664c4fa620de1fc2cd8
Psicometrix <38756223+psicometrix@users.noreply.github.com>
2020-07-28 14:35:43 -0400

    Cambios generales para Landing Psicometrix
    Se agrego funcionalidad que detecta el dia para el contador de hora de activacion.
    
    Se agrego logo y el background.
    
    Se agregaron las redes sociales y se cambio elcolor de la fuente.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LandingPage
+
+Esta es una pagina que peude ser usuada para diferentes funcionaleidades de informacion.
--- js/main.js
+++ js/main.js
@@ -51,17 +51,28 @@ $(window).load(function(){
 		setTimeout(function(){window.scrollTo(0,0);},0);
 	})();
 
+	
+
 });
 /* END ------------------------------------------------------- */
 
+function getDateNow(){
+	var date = new Date;
+	var nowDate = [
+		date.getFullYear(),
+		date.getMonth() + 1,
+		date.getDate()
+	]
+	return nowDate.join('/') + " 18:00:00";
+}
 
 $('#countdown').countdown({
-	date: "Mar 25 2021",
+	date: getDateNow(),
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
 	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+	  
 	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
 	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
 	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
